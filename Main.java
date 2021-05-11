05d50ba6090f3453eb60495b7b8507adc8f34593
Roman <novikovroman96@gmail.com>
2021-05-11 20:53:56 +0300

    Add homework to lesson 4

--- Main.java
+++ Main.java
@@ -1,55 +1,23 @@
-package ru.geekbrains.level_2.lesson_3;
+package sample;
 
-/*
-1. Создать массив с набором слов (10-20 слов, должны встречаться повторяющиеся). Найти и вывести список уникальных слов,
-из которых состоит массив (дубликаты не считаем). Посчитать, сколько раз встречается каждое слово.
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-2. Написать простой класс Телефонный Справочник, который хранит в себе список фамилий и телефонных номеров. В этот
-телефонный справочник с помощью метода add() можно добавлять записи, а с помощью метода get() искать номер телефона по
-фамилии. Следует учесть, что под одной фамилией может быть несколько телефонов (в случае однофамильцев), тогда при
-запросе такой фамилии должны выводиться все телефоны. Желательно не добавлять лишний функционал (дополнительные поля
-(имя, отчество, адрес), взаимодействие с пользователем через консоль и т.д). Консоль использовать только для вывода
-результатов проверки телефонного справочника.
- */
+public class Main extends Application {
 
-import java.util.*;
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 500, 700));
+        primaryStage.show();
+    }
 
-public class Main {
-    public static void main(String[] args){
 
-        System.out.println("Task 1:\n");
-
-        List<String> words = Arrays.asList(
-                "Python", "JavaScript", "Java", "C++", "Golang",
-                "C#", "SQL", "Dart", "C++", "Golang",
-                "JavaScript", "Audi", "C++", "C#", "Golang",
-                "Python", "JavaScript", "Java", "Java", "SQL"
-        );
-
-        Set<String> unique = new HashSet<>(words);
-
-        System.out.println("Initial array:");
-        System.out.println(words);
-
-        System.out.println("Unique words:");
-        System.out.println(unique);
-
-        System.out.println("Frequency:");
-        for (String key : unique) {
-            System.out.println(key + ": " + Collections.frequency(words, key) + "\n");
-        }
-
-        System.out.println("Task 2:\n");
-
-        Phonebook phonebook = new Phonebook();
-        List<String> surnames = Arrays.asList("Smith", "Johnson", "Williams", "Brown", "Jones", "Miller");
-        List<String> phones = Arrays.asList("202-555-0146", "202-555-0129", "202-555-0157", "202-555-0198", "202-555-0103");
-
-        for (int i = 0, j = 0; i < surnames.size() && j < phones.size(); i++, j++) {
-            phonebook.add(surnames.get(i), phones.get(j));
-        }
-
-        System.out.println(phonebook.get("Smith"));
-        System.out.println(phonebook.get("Miller"));
+    public static void main(String[] args) {
+        launch(args);
     }
 }
